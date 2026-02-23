2311fbcd121dfdda0026878689c2fd1b51d48f8d
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-02-23 14:57:47 +0200

    mettre le fonctions apres injectation de element html en js

--- frontend/js/single_page.js
+++ frontend/js/single_page.js
@@ -1,108 +1,110 @@
-// let main_section = document.querySelector('.contents');
+let main_section = document.querySelector('.contents');
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     main_section.innerHTML =
-//         `
-//         <div class="relation">
-//        <form action="/api/commissions/relations" method="POST">
-//         <h2>Ajouter une relation</h2>
+document.addEventListener('DOMContentLoaded', () => {
+    main_section.innerHTML =
+        `
+        <section class="achats">
+            <h2>
+                <i class="fi fi-rr-user"></i>
+                Ajouter un client
+            </h2>
+            <form action="/api/clients" method="post">
+                <input type="text" id="clientName" name="nom" placeholder="Nom du client">
+                <button type="submit">Ajouter</button>
+            </form>
+        </section>
+    `
 
-//         <label>Parrain </label><br>
-//         <select name="parrain_id" id="clients"></select><br><br>
+})
 
-//         <label>Filleul </label> <br>
-//         <select name="filleul_id" id="filleulSelect"></select> <br><br>
+function showPages(content) {
 
-//         <button type="submit">Lier les deux</button>
-//     </form>
-//     </div>
-//     `
+    if (content == "ajouter-relation") {
+        main_section.innerHTML =
+            `
+          <div class="relation">
+       <form action="/api/commissions/relations" method="POST">
+        <h2>Ajouter une relation</h2>
 
-// })
+        <label>Parrain </label><br>
+        <select name="parrain_id" id="clients"></select><br><br>
 
-// function showPages(content) {
+        <label>Filleul </label> <br>
+        <select name="filleul_id" id="filleulSelect"></select> <br><br>
 
-//     if (content == "ajouter-relation") {
-//         main_section.innerHTML =
-//             `
-//           <div class="relation">
-//        <form action="/api/commissions/relations" method="POST">
-//         <h2>Ajouter une relation</h2>
+        <button type="submit">Lier les deux</button>
+    </form>
+    </div>
+        `
+    }
+    else if (content == "ajouter-achat") {
+        main_section.innerHTML =
+            `
+         <div class="relation">
+        <form action="/api/commissions/achats" method="POST">
+            <h2>Ajouter un achat</h2>
+            <label>Client qui achète :</label><br>
+            <select name="client_id" id="buyerSelect"></select><br><br> 
+            <input type="number" name="montant"
+                placeholder="Montant ($)" required>
+                <br><br>
+            <button type="submit">Enregistrer l'achat</button>
+        </form>
+    </div>
+        `
+    }
+    else if (content == "afficher-relations") {
+        main_section.innerHTML =
+            `
+            <section class="display">
+                <div class="clientsBar">
+                    <div>
+                        <h3>Clients</h3>
+                        <ul id="clientsList"></ul>
+                    </div>
+                </div>
+                <div>
+                    <h3>Relations</h3>
+                    <ul id="relationsList"></ul>
+                </div>
 
-//         <label>Parrain </label><br>
-//         <select name="parrain_id" id="clients"></select><br><br>
+                <div>
+                    <h3>Achats</h3>
+                    <ul id="achatsList"></ul>
+                </div>
+            </section>
+        `
+    }
+    else if (content == "afficher-details") {
+        main_section.innerHTML =
+            `
+            <div class="client_list">
+                <h3>Clients</h3>
+                <ul id="clientsList"></ul>
+            </div>
+            <section class="details">
+                <h2>Détails du client</h2>
+                <p><strong>Client sélectionné :</strong> <span id="clientNom">Aucun</span></p>
+                <p>Filleuls directs : <span id="directs">-</span></p>
+                <p>Filleuls indirects : <span id="indirects">-</span></p>
+                <p>Total commissions : <span id="commissions">0 $</span></p>
+            </section>
+        `
+    }
+    else if (content == "voir-graphique") {
+        main_section.innerHTML =
+            `
+        <section class="graph">
+  <h2>Réseau graphique de parrainage</h2>
+  <div id="network"
+       style="width:100%; height:700px; border:1px solid #ccc;">
+  </div>
+</section>
+        `
+    }
 
-//         <label>Filleul </label> <br>
-//         <select name="filleul_id" id="filleulSelect"></select> <br><br>
 
-//         <button type="submit">Lier les deux</button>
-//     </form>
-//     </div>
-//         `
-//     }
-//     else if (content == "ajouter-achat") {
-//         main_section.innerHTML =
-//             `
-//          <div class="relation">
-//         <form action="/api/commissions/achats" method="POST">
-//             <h2>Ajouter un achat</h2>
-//             <label>Client qui achète :</label><br>
-//             <select name="client_id" id="buyerSelect"></select><br><br> 
-//             <input type="number" name="montant"
-//                 placeholder="Montant ($)" required>
-//                 <br><br>
-//             <button type="submit">Enregistrer l'achat</button>
-//         </form>
-//     </div>
-//         `
-//     }
-//     else if (content == "afficher-relations") {
-//         main_section.innerHTML =
-//             `
-//             <section class="display">
-//                 <div class="clientsBar">
-//                     <div>
-//                         <h3>Clients</h3>
-//                         <ul id="clientsList"></ul>
-//                     </div>
-//                 </div>
-//                 <div>
-//                     <h3>Relations</h3>
-//                     <ul id="relationsList"></ul>
-//                 </div>
 
-//                 <div>
-//                     <h3>Achats</h3>
-//                     <ul id="achatsList"></ul>
-//                 </div>
-//             </section>
-//         `
-//     }
-//     else if (content == "afficher-details") {
-//         main_section.innerHTML =
-//             `
-//             <div class="client_list">
-//                 <h3>Clients</h3>
-//                 <ul id="clientsList"></ul>
-//             </div>
-//             <section class="details">
-//                 <h2>Détails du client</h2>
-//                 <p><strong>Client sélectionné :</strong> <span id="clientNom">Aucun</span></p>
-//                 <p>Filleuls directs : <span id="directs">-</span></p>
-//                 <p>Filleuls indirects : <span id="indirects">-</span></p>
-//                 <p>Total commissions : <span id="commissions">0 $</span></p>
-//             </section>
-//         `
-//     }
-//     else if (content == "voir-graphique") {
-//         main_section.innerHTML =
-//             `
-//         <section class="graph">
-//   <h2>Réseau graphique de parrainage</h2>
-//   <div id="network"
-//        style="width:100%; height:700px; border:1px solid #ccc;">
-//   </div>
-// </section>
-//         `
-//     }
-// }
+    loadAllData();
+    chargerGraphe();
+}
